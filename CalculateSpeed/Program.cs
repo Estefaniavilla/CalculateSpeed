3ca61d23924f93a4e02723e6cee808630b2f0130
estefivilla07@gmail.com <estefivilla07@gmail.com>
2024-02-19 21:43:54 -0600

    Agregar archivos de proyecto.

---
+++ CalculateSpeed/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Enter the distance traveled (in meters): ");
+        float distance = float.Parse(Console.ReadLine());
+
+        Console.Write("Enter the time taken (in seconds): ");
+        float time = float.Parse(Console.ReadLine());
+
+        // fórmula v = d/t
+        float speed = distance / time;
+
+        
+        Console.WriteLine($"The speed is: {speed} [m/s]");
+    }
+}
